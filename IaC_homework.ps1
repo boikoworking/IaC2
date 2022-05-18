fbe6e15ccc80228e68a9a0411bcc0a37531a8c70
boikoworking <boiko.working@gmail.com>
2022-05-18 20:32:35 +0300

    commit5
    added defautValue of storageprefix in main.json and storage_account.json

--- IaC_homework.ps1
+++ IaC_homework.ps1
@@ -2,5 +2,5 @@
 az account set --subscription 23f5f8a4-4a06-4dce-92e7-6b980f7ad0b8
 $templateFile = "https://raw.githubusercontent.com/boikoworking/IaC1/main/main.json"
 $devParameterFile = "https://raw.githubusercontent.com/boikoworking/IaC1/main/parametrs.json"
-az group create -n myResourceGroupDev3  -l eastus
-az deployment group create --debug -g myResourceGroupDev1 -n devenvironment -u $templateFile -p $devParameterFile
+az group create -n myResourceGroupDev6  -l eastus
+az deployment group create --debug -g myResourceGroupDev6 -n devenvironment -u $templateFile -p $devParameterFile
