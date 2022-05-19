45c8b686f17e8c2910485a05796d4b72406f8200
boikoworking <boiko.working@gmail.com>
2022-05-19 21:06:56 +0300

    update

--- IaC_homework.ps1
+++ IaC_homework.ps1
@@ -2,5 +2,5 @@
 az account set --subscription 23f5f8a4-4a06-4dce-92e7-6b980f7ad0b8
 $templateFile = "https://raw.githubusercontent.com/boikoworking/IaC1/main/main.json"
 $devParameterFile = "https://raw.githubusercontent.com/boikoworking/IaC1/main/parametrs.json"
-az group create -n myResourceGroupDev9 -l eastus
-az deployment group create --name devenvironment --resource-group myResourceGroupDev9 --template-uri $templateFile --parameters $devParameterFile --debug
+az group create -n myResourceGroupDev10 -l eastus
+az deployment group create --name devenvironment --resource-group myResourceGroupDev10 --template-uri $templateFile --parameters $devParameterFile --debug
