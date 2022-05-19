3c099595c3b90b4e9a7d77061a17a7c8a14460ee
boikoworking <boiko.working@gmail.com>
2022-05-19 11:15:17 +0300

    commit
    updated parametrs.json and storage_account.json

--- IaC_homework.ps1
+++ IaC_homework.ps1
@@ -1,5 +1,6 @@
 ﻿az login
 az account set --subscription 23f5f8a4-4a06-4dce-92e7-6b980f7ad0b8
-$templateFile = "https://raw.githubusercontent.com/boikoworking/IaC1/main/main.json"
-Start-Sleep -Seconds 200
-az deployment group create --name devenvironment --resource-group myResourceGroupDe8 --template-uri $templateFile --debug
+$templateFile = "https://raw.githubusercontent.com/boikoworking/IaC1/main/linked/storage_account.json"
+$devParameterFile = "https://raw.githubusercontent.com/boikoworking/IaC1/main/parametrs.json"
+az group create -n myResourceGroupDev -l eastus
+az deployment group create --name devenvironment --resource-group myResourceGroupDev --template-uri $templateFile --parameters $devParameterFile --debug
